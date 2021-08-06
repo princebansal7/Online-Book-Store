1512abdb93f92f9bb7bb4cc75e2711f0b16d2ad5
Prince <75045465+princebansal7@users.noreply.github.com>
2021-08-06 17:37:53 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,79 @@
+<?php session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<?php
+			include("includes/head.inc.php");
+		?>
+</head>
+
+<body>
+			<!-- start header -->
+				<div id="header">
+					<div id="menu">
+						<?php
+							include("includes/menu.inc.php");
+						?>
+					</div>
+				</div>
+				
+				<div id="logo-wrap">
+					<div id="logo">
+							<?php
+								include("includes/logo.inc.php");
+							?>
+					</div>
+				</div>
+			<!-- end header -->
+			
+			<!-- start page -->
+
+				<div id="page">
+					<!-- start content -->
+					<div id="content">
+						<div class="post">
+							<h1 class="title">About Us</h1>
+							<div class="entry" style="height:auto">
+								
+We <a href="index.php">American Book Company </a>is the largest wholesale distributor of closeout, remainder, hurt, promotional and bargain-priced books. “Remainder” books are overstocks that have not left the publisher’s warehouse. “Hurt” books are not necessarily damaged or defective, but refer to books returned to publishers which are not reprocessed into inventory. 
+
+Our company was founded in 1997 by Dean Winegardner who is currently our Chairman. We source our books primarily from leading publishers such as Penguin Putnam, Harper Collins, Random House, Hachette Book Group, Perseus, and St. Martins Press as well as other publishers, book brokers and distributors. We have a variety of customers including bookstores, supermarkets, drug and dollar stores, book fairs, discount chains, colleges and more. Currently, we have our sales office in Knoxville, TN and distribution center in Jefferson City, TN of over 900,000 sq. ft. 
+
+We offer high quality titles, including many former bestsellers at tremendous values. And each week, our inventory is added to and replenished by the hundreds, so our customers always have access to bestselling books from bestselling authors. In addition, we have more than 20,000 titles to choose from, across more than 60 different broad and niche categories. We offer a number of formats including hardcover novel, trade paper, paperback, coffee table and audio books. Accessories, games and mini books are part of our inventory as well. And our products are offered at some of the lowest prices in the industry. 
+
+Although our books have been subject to slight wear from shipping and shelf life, most are in good to excellent condition. “Mint Condition” can be requested for a higher price.
+
+Our custom-built display units are shipped upon request with each order, allowing for easy set up of promotions. Therefore, our customers do not have to worry about making space on their shelves. We offer unique displays that house various formats of books. 
+
+We ship most title orders within two weeks. We have one of the quickest turnovers in the industry because we realize that timing is an important part of promotions. And we provide special services upon request. We can label books with either a retail price point or UPC barcode. We are EDI capable and perform order assorting, drop shipments and customized assortments. We will even route orders for our accounts. In addition, our customers can take advantage of volume freight discounts by using our traffic department.
+ <br/>
+
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- end content -->
+					
+					<!-- start sidebar -->
+					<div id="sidebar">
+							<?php
+								include("includes/search.inc.php");
+							?>
+					</div>
+					<!-- end sidebar -->
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+			<!-- end page -->
+			
+			<!-- start footer -->
+				<div id="footer">
+							<?php
+								include("includes/footer.inc.php");
+							?>
+				</div>
+			<!-- end footer -->
+</body>
+</html>
